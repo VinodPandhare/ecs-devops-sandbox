2a3dc8818414feabff19f8a96390bd90306b2b8c
VinodPandhare <150361812+VinodPandhare@users.noreply.github.com>
2024-07-15 18:48:45 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -5,7 +5,11 @@ class TestApp(unittest.TestCase):
     def test_return_backwards_string(self):
         random_string = "This is my test string"
         random_string_reversed = "gnirts tset ym si sihT"
-        self.assertEqual(random_string_reversed, returnBackwardsString(random_string))
+        try:
+            self.assertEqual(random_string_reversed, returnBackwardsString(random_string))
+        except AssertionError as e:
+            print(f"AssertionError: {e}")
+            raise
 
 if __name__ == "__main__":
     unittest.main()
