01ad6ae88f19b4185be91e793f9cba3bdb40ea88
VinodPandhare <150361812+VinodPandhare@users.noreply.github.com>
2024-07-15 17:13:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import sys
 # Adjusting Python path to include ecs_devops_sandbox_cdk directory
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'ecs_devops_sandbox_cdk')))
 
-def returnBackwardsString(input_string):
-    # Your implementation to reverse the input string
-    return input_string[::-1]
-
 import aws_cdk as cdk
 from ecs_devops_sandbox_cdk.ecs_devops_sandbox_cdk_stack import EcsDevopsSandboxCdkStack
 
